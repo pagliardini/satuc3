27dc277d749dcb4d2c6aa9f018587bd3c59aca23
Alan García Pagliardini <alan.garcia@ucc.local>
2025-04-16 15:19:46 -0300

    marcas

--- app.py
+++ app.py
@@ -1,18 +1,29 @@
 from flask import Flask, render_template
-from impresoras import impresoras_bp  # Importar el blueprint
+from models import db  # Importar db desde models.py
+from impresoras import impresoras_bp
 from insumos import insumos_bp
 from ups import ups_bp
 
 app = Flask(__name__)
 
+# Configuración de SQLAlchemy
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+app.config['SECRET_KEY'] = 'your_secret_key_here'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+# Inicializar db con la aplicación Flask
+db.init_app(app)
+
+# Registrar blueprints
 app.register_blueprint(impresoras_bp)
 app.register_blueprint(insumos_bp)
 app.register_blueprint(ups_bp)
-
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()  # Crear tablas si no existen
     app.run(debug=True)
--- create_db.py
+++ create_db.py
@@ -1,12 +1,5 @@
+from app import app
 from models import db
-from flask import Flask
-
-app = Flask(__name__)
-
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'  
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-
-db.init_app(app)
 
 with app.app_context():
     db.create_all()
